ee032f70f117dfea3123a89725112fcc269bdc76
Jhoie <victoriousjvictor@gmail.com>
2025-11-19 00:46:55 +0100

    Fixed repetition in API pagination script

--- script.py
+++ script.py
@@ -5,36 +5,37 @@ import requests
 from dotenv import load_dotenv
 
 
-def api_paginator(url, params, page_counter):
+def api_paginator(url, params):
     response = requests.get(url, params)
+
     if response.status_code == 200:
         data = response.json()
         all_results = []
 
-        while data['response']['pages'] > page_counter:
-            page_counter += 1
-            all_results.extend(data['response']['results'])
+        while params["page"] <= data['response']['pages']:
+            next_page = requests.get(url, params)
+            more_news = next_page.json()
+
+            all_results.extend(more_news['response']['results'])
+            params["page"] += 1
 
         dataset = pd.DataFrame(all_results)
         return dataset
 
     else:
-        raise ValueError(f"Failed with status code: {response.status_code}")
+        response.raise_for_status()
 
 
 load_dotenv()
-
-API_KEY = os.environ.get("API_KEY")
 BASE_URL = os.environ.get("BASE_URL")
-page_counter = 1
 
 params = {
     "q": "AI",
-    "page": page_counter,
+    "page": 1,
     "tag": "technology/google",
     "from-date": "2025-09-01",
-    "api-key": API_KEY
+    "api-key": os.environ.get("API_KEY")
 }
 
-dataframe = api_paginator(BASE_URL, params, page_counter)
+dataframe = api_paginator(BASE_URL, params)
 print(dataframe)
