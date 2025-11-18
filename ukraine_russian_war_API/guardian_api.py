eb9ea2be52beab247cd104ca51b5654820a52636
Chidinma <chidinmaokeh75@gmail.com>
2025-11-18 23:40:13 +0100

    use flake8 and isort to format my code

--- ukraine_russian_war_API/guardian_api.py
+++ ukraine_russian_war_API/guardian_api.py
@@ -2,10 +2,7 @@
 import json
 import os
 
-import flake8
-import isort
 import requests
-from requests.exceptions import RequestException
 
 
 def save_to_json(articles, filename="guardian_articles.json"):
@@ -16,30 +13,25 @@ def save_to_json(articles, filename="guardian_articles.json"):
 
 
 def main():
- 
     api_key = os.getenv("GUARDIAN_API_KEY")
 
     if not api_key:
-        raise ValueError("ERROR: Environment variable GUARDIAN_API_KEY not set.")
+        raise ValueError("ERROR: GUARDIAN_API_KEY not set.")
 
     end_point_url = "https://content.guardianapis.com/search"
 
-
     params = {
-        "api-key": api_key,        
-        "q": "Russia Ukraine war",      
+        "api-key": api_key,
+        "q": "Russia Ukraine war",
         "page-size": 50,
         "from-date": "2024-01-01",
-        "to-date": "2025-01-30", 
-        "page": 1  
+        "to-date": "2025-01-30",
+        "page": 1
     }
 
     print("Sending first request to Guardian API...")
 
     response = requests.get(end_point_url, params=params)
-
-  
-    
     if response.status_code != 200:
         print("API returned an error:", response.status_code)
         raise Exception("Stopping execution due to API error.")
@@ -48,19 +40,12 @@ def main():
         data = response.json()
     except Exception:
         raise ValueError("Response is not valid JSON.")
-
-
-    
     if "response" not in data or "pages" not in data["response"]:
         raise KeyError("Missing 'response' or 'pages' in API data.")
 
     total_pages = data["response"]["pages"]
     print(f"Total pages available: {total_pages}")
-
-  
     all_articles = []
-
-    
     for page in range(1, total_pages + 1):
         print(f"Fetching page {page} of {total_pages}...")
 
@@ -85,9 +70,9 @@ def main():
 
     print(f"Finished! Total articles fetched: {len(all_articles)}")
 
-
     save_to_json(all_articles, "guardian_articles.json")
 
 
 if __name__ == "__main__":
+
     main()
