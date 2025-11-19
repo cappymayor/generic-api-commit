b114bbe761ec84cb0f64d2a61fd2f6849a2e08f6
Mac <mac@2Q-L095Q.local>
2025-11-19 13:36:33 +0100

    Modify from class and added .gitignore

--- scripts/guardian-api-data.py
+++ scripts/guardian-api-data.py
@@ -27,77 +27,67 @@ argparser.add_argument(
     )
 
 
-class TheGuardianAPI:
+# Extract all pages from the URL and return as a pandas DataFrame
+# after selecting fields of interest
+def get_articles_dataframe(
+    base_url, api_key, request_parameters, max_pages=None
+):
     """
-    A class to interact with The Guardian API and fetch articles.
+    Fetch articles from The Guardian API and return as a pandas DataFrame.
 
-    Attributes:
-        params (dict): Parameters for API requests.
+    Args:
+        base_url (str): The base URL of the API.
+        api_key (str): The API key for authentication.
+        query (str): The search query.
+        page_size (int): Number of articles per page.
+        max_pages (int, optional): Maximum number of pages to fetch.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the articles.
     """
-    def __init__(self, **params):
-        self.params = params
+    all_articles = []
+    page = 1
+    while True:
+        params = request_parameters.copy()
+        params["page"] = page
+        url = f"{base_url}?api-key={api_key}"
+        try:
+            response = requests.get(url, params=params)
+        except Exception as e:
+            print(f"An error occurred while making the request: {e}")
+            raise
+        if response.status_code != 200:
+            print(f"API request failed with status {response.status_code}")
+            break
 
-    # Extract all pages from the URL and return as a pandas DataFrame
-    # after selecting fields of interest
-    def get_articles_dataframe(
-        self, base_url, api_key, request_parameters, max_pages=None
-    ):
-        """
-        Fetch articles from The Guardian API and return as a pandas DataFrame.
+        max_page = response.json().get("response", {}).get("pages", 0)
+        if max_pages is not None and max_pages < max_page:
+            max_page = max_pages
+        data = response.json()
+        articles = data.get("response", {}).get("results", [])
+        if not articles:
+            break
 
-        Args:
-            base_url (str): The base URL of the API.
-            api_key (str): The API key for authentication.
-            query (str): The search query.
-            page_size (int): Number of articles per page.
-            max_pages (int, optional): Maximum number of pages to fetch.
+        for article in articles:
+            fields = article.get("fields", {})
+            formatted_article = {
+                "headline": fields.get("headline", ""),
+                "trailText": fields.get("trailText", ""),
+                "byline": fields.get("byline", ""),
+                "publication": fields.get("publication", ""),
+                "webUrl": article.get("webUrl", ""),
+                "webPublicationDate": article.get(
+                    "webPublicationDate", ""
+                ),
+            }
+            all_articles.append(formatted_article)
 
-        Returns:
-            pd.DataFrame: A DataFrame containing the articles.
-        """
-        all_articles = []
-        page = 1
-        while True:
-            params = request_parameters.copy()
-            params["page"] = page
-            url = f"{base_url}?api-key={api_key}"
-            try:
-                response = requests.get(url, params=params)
-            except Exception as e:
-                print(f"An error occurred while making the request: {e}")
-                raise
-            if response.status_code != 200:
-                print(f"API request failed with status {response.status_code}")
-                break
+        print(f"Fetched page {page} of {max_page}")
+        page += 1
+        if page > max_page:
+            break  # Exiting the loop if we've reached the max pages
 
-            max_page = response.json().get("response", {}).get("pages", 0)
-            if max_pages is not None and max_pages < max_page:
-                max_page = max_pages
-            data = response.json()
-            articles = data.get("response", {}).get("results", [])
-            if not articles:
-                break
-
-            for article in articles:
-                fields = article.get("fields", {})
-                formatted_article = {
-                    "headline": fields.get("headline", ""),
-                    "trailText": fields.get("trailText", ""),
-                    "byline": fields.get("byline", ""),
-                    "publication": fields.get("publication", ""),
-                    "webUrl": article.get("webUrl", ""),
-                    "webPublicationDate": article.get(
-                        "webPublicationDate", ""
-                    ),
-                }
-                all_articles.append(formatted_article)
-
-            print(f"Fetched page {page} of {max_page}")
-            page += 1
-            if page > max_page:
-                break  # Exiting the loop if we've reached the max pages
-
-        return pd.DataFrame(all_articles)
+    return pd.DataFrame(all_articles)
 
 
 def load_params(file_path):
@@ -118,7 +108,6 @@ def main():
         raise ValueError("The --params argument is required.")
     params = load_params(args.params)
     max_pages = args.max_pages
-    guardian_job = TheGuardianAPI()
     if "config" not in params or "request_params" not in params:
         raise KeyError("The JSON file must contain a 'config' or "
                        "'request_params' key.")
@@ -127,7 +116,7 @@ def main():
     if not api_key:
         raise ValueError("API_KEY is not set in the environment variables.")
 
-    df = guardian_job.get_articles_dataframe(
+    df = get_articles_dataframe(
         base_url=config["base_url"],
         api_key=api_key,
         request_parameters=params["request_params"],
