c072f15ef97f5e2955ebd6a328270159eaa09884
Ayodeji <aayo.agunbiade@gmail.com>
2025-11-21 18:02:07 +0100

    Russia-Ukraine Data Extractor

--- api_data_extractor.py
+++ api_data_extractor.py
@@ -1,61 +1,45 @@
-import time
-
 import requests
+
+import pandas as pd
 
 import access_key
 
+url = "https://content.guardianapis.com/search"
+current_page = 1
+total_pages = 1
 
-def fetch_all_guardian_data(query="Russian OR Ukraine"):
+container = []
 
-
-    base_url = "https://content.guardianapis.com/search"
-    all_results = []
-
-    params = {
-        "q": query,
-        "api-key": access_key.api_key,
-        "page": 1,        
-        "page-size": 50
+while current_page <= total_pages:
+    params ={
+    "api-key": access_key.api_key,
+    "q": "Russia Ukraine",
+    "from-date": "2025-10-11",
+    "to-date": "2025-10-20",
+    "page": current_page
+    
     }
+    
+    response = requests.get(url=url, params=params)
+    
+    if response.status_code != 200:
+        print(response.raise_for_status())
+        break
+        
+    else:
+        full = response.json()
+        total_pages = full['response']['pages']
+        
+        
+        total_results = full['response']['results']
+        for data in total_results:
+            container.append(data)
+        
+        
+        current_page += 1
+        
+print(len(container))
 
-    total_pages = 1       
-    current_page = 1      # loop counter
+df = pd.json_normalize(container)
+df.to_csv("data/Russ_Ukr_war_Update.csv")
 
-    print(f"Starting data retrieval for query: '{query}'")
-
-
-    while current_page <= total_pages:
-        params['page'] = current_page
-
-        if current_page > 1:
-            time.sleep(0.5) 
-
-        print(f"Retrieving page {current_page}...")
-
-        try:
-            r = requests.get(base_url, params=params)
-            r.raise_for_status() 
-            page_data = r.json()
-
-            response = page_data['response']
-
-            if current_page == 1:
-                total_pages = response.get('pages', 1)
-                print(f"Discovered total pages: {total_pages}")
-
-
-            all_results.extend(response.get('results', []))
-
-
-            current_page += 1
-
-        except requests.exceptions.RequestException as error:
-            print(f"Request failed on page {current_page}: {error}.")
-            break 
-
-    print(f"\nFinished retrieval! Total articles collected: {len(all_results)}")
-    return all_results
-
-if __name__ == '__main__':
-    print("Running extractor directly.")
-    data = fetch_all_guardian_data()
