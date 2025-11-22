ef1a3aca6e36d2fdde0cc5c0b881c75d175f3779
Chidinma <chidinmaokeh75@gmail.com>
2025-11-22 18:14:08 +0100

    use flake8 to format my code

--- API Project/Random API/user.py
+++ API Project/Random API/user.py
@@ -7,19 +7,19 @@ url = "https://randomuser.me/api/?results=100"
 def female():
     response = requests.get(url)
     data = response.json()
-    data.keys() 
+    data.keys()
+
     results = data["results"]
 
     females = []
     for i in results:
-      if i["gender"] == "female" and i["dob"]["age"] < 43:
-        females.append(i)
+        if i["gender"] == "female" and i["dob"]['age'] < 43:
+            females.append(i)
+    print(females)
 
-    data= pd.json_normalize(females)
+    print(f"Finished! Total females fetched: {len(females)}")
 
-    return data
-
-
-dataframe = female()
-
-print(dataframe)
+    data = pd.json_normalize(females)
+    df = pd.DataFrame(data)
+    print(df)
+    female()
