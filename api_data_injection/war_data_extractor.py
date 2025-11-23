deb6ee27574abecbca367b025d03318ca20dce33
Ayodeji <aayo.agunbiade@gmail.com>
2025-11-24 00:24:20 +0100

    Russia/Ukraine war data extraction file

--- api_data_injection/war_data_extractor.py
+++ api_data_injection/war_data_extractor.py
@@ -1,7 +1,7 @@
+import os
+
 import pandas as pd
 import requests
-
-import access_key
 
 url = "https://content.guardianapis.com/search"
 current_page = 1
@@ -10,33 +10,28 @@ total_pages = 1
 container = []
 
 while current_page <= total_pages:
-    params ={
-    "api-key": access_key.api_key,
+    params={
+    "api-key": os.environ.get('API_KEY'),
     "q": "Russia Ukraine",
     "from-date": "2025-10-11",
     "to-date": "2025-10-20",
     "page": current_page
-    
-    }
-    
+           }
+
     response = requests.get(url=url, params=params)
-    
     if response.status_code != 200:
-        print(response.raise_for_status())
-        break
-        
+        response.raise_for_status()
+
     else:
         full = response.json()
         total_pages = full['response']['pages']
-        
-        
+
         total_results = full['response']['results']
         for data in total_results:
             container.append(data)
-        
-        
+
         current_page += 1
-        
+
 print(f"Total recored processed: {len(container)}")
 
 df = pd.json_normalize(container)
