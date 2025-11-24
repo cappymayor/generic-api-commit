1f8d5369672d5552f67702bf8073149b3fbe912d
Chidinma <chidinmaokeh75@gmail.com>
2025-11-24 15:24:47 +0100

    modify my code

--- API Project/Guardian API/guardian_api.py
+++ API Project/Guardian API/guardian_api.py
@@ -1,60 +1,64 @@
-import json
+import logging
 import os
 
+import pandas as pd
 import requests
 
-BASEURL = "https://content.guardianapis.com/search"
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.INFO)
+
+BASE_URL = "https://content.guardianapis.com/search"
+API_KEY = os.getenv("GUARDIAN_API_KEY")
+all_articles = []
 
 
 def guardian_api():
-    api_key = os.getenv("GUARDIAN_API_KEY")
-    if api_key is None:
+    """
+   The function fetches OpenAI news from The Guardian API,
+    handles pagination, normalizes JSON, and returns a DataFrame.
+    """
+    page = 1
+
+    if API_KEY is None:
         raise ValueError("ERROR: GUARDIAN_API_KEY is missing")
 
     params = {
-        "api-key": api_key,
-        "q": "fashion",
+        "api-key": API_KEY,
+        "q": "OpenAI",
         "page-size": 50,
         "from-date": "2025-01-01",
         "to-date": "2025-01-30",
-        "page": 1
+        "page": page
     }
 
-    response = requests.get(BASEURL, params=params)
-    if response.status_code == 401:
-        raise ValueError("Invalid API key")
-    if response.status_code == 400:
-        raise TypeError("invalid syntax")
+    response = requests.get(BASE_URL, params=params)
+
     if response.status_code != 200:
         raise Exception(f"Error: {response.status_code}")
 
-    first_page = response.json()
-    total_pages = first_page["response"].get("pages", 1)
-    print(f"Total pages available: {total_pages}")
+    raw_response = response.json()
+    total_pages = raw_response["response"].get("pages", 1)
 
-    all_articles = []
-
-    page = 1
+    logging.info(f"Total pages found: {total_pages}")
     while page <= total_pages:
-        print(f"Fetching page {page}...")
-
+        logging.info(f"Fetching page {page}...")
         params["page"] = page
-        response = requests.get(BASEURL, params=params)
+        response = requests.get(BASE_URL, params=params)
 
         data = response.json()
         results = data["response"]["results"]
-
         all_articles.extend(results)
         page += 1
 
-    print(f"Finished! Total articles fetched: {len(all_articles)}")
+    logging.info(f"Finished! Total articles fetched: {len(all_articles)}")
 
-    # Save JSON file
-    with open("guardian_articles.json", "w", encoding="utf-8") as f:
-        json.dump(all_articles, f, indent=4, ensure_ascii=False)
+    df = pd.json_normalize(all_articles)
+    df.to_json("guardian_openai_news.json", orient="records", indent=4)
+    logging.info("Normalized dataset saved as guardian_openai_news.json")
 
-    print("Saved guardian_articles.json successfully.")
+    return df
 
 
-# Run the script
-guardian_api()
+if __name__ == "__main__":
+
+    guardian_api()
